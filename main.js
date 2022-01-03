e04f8bd1fff52079656e20dc782a077f2f5e5446
Sanvi-Agarwal <85011956+Sanvi-Agarwal@users.noreply.github.com>
2022-01-03 16:29:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,27 @@ function start() {
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
     object_name = document.getElementById("object_name").value;
 }
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            nofill();
+            stroke(#FF0000);
+            rect(objects[i].x, objects[i].width, objects[i].height);
+        }
+    }
+}
